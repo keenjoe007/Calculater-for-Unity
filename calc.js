378a1797e70699091020980fcc65b3a42f747cc1
jun <doglovefalcon@gmail.com>
2013-10-25 15:53:00 +0900

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,117 @@
+#pragma strict
+
+var partsNumbers = new Array();	//parts of targetsNum
+var targets = new Array();	//targets of operation
+var math = new Array();	// + or - or * or /
+var answer:int = 0;
+
+
+/////////createButtons//////////
+function OnGUI(){
+
+	var setNumButton = function(x,y,num){
+		if(GUI.Button(Rect(x,y,100,50),num.ToString())){
+			inputPartsNum(num);
+		}
+	};
+	var setMathButton = function(y,mth){
+		if(GUI.Button(Rect(410,y,50,50),mth.ToString())){
+			if(partsNumbers.length>0){
+				createTargetNum();
+				resetPartsNum();
+				inputMath(mth);
+			}else{
+				Debug.Log("you have no targets!");
+			}
+		}
+	};
+	var setAnswerButton = function(){
+		if(GUI.Button(Rect(210,210,200,50),"=")){
+			if(partsNumbers.length>0){
+			
+				createTargetNum();
+				calculation();
+				
+				resetPartsNum();
+				resetTargets();
+				resetMath();
+			}else{
+				Debug.Log("I can't calculate now!");
+			}
+		}
+	};
+	
+	for(var i=1;i<10;i++){
+		if(i<=3){
+			setNumButton(10+100*i,60,i);					
+		}else if(i<=6){
+			setNumButton(10+100*(i-3),110,i);		
+		}else{
+			setNumButton(10+100*(i-6),160,i);		
+		}
+	}
+	setNumButton(110,210,0);
+	
+	setMathButton(60,"+");
+	setMathButton(110,"-");
+	setMathButton(160,"*");
+	setMathButton(210,"/");
+	
+	setAnswerButton();
+	
+	GUI.Label(Rect(110,10,350,50),answer.ToString());
+}
+////////////////////////////////////
+
+//////////input///////////////////
+function inputPartsNum(num){
+	partsNumbers.Push(num);
+}
+
+function inputMath(mth){
+	math.Push(mth);
+}
+//////////////////////////////////
+
+/////////createTargets///////////
+function createTargetNum(){
+	var createdNum:int = 0;
+	partsNumbers.Reverse();
+	for(var i=0;i<partsNumbers.length;i++){
+		var part:int = partsNumbers[i];
+		createdNum += part*Mathf.Pow(10,i);
+	}
+	targets.Push(createdNum);
+	Debug.Log(createdNum + " has created!");
+}
+/////////////////////////////////
+
+/////////resetArrays////////////
+function resetPartsNum(){
+	partsNumbers = new Array();
+}
+function resetTargets(){
+	targets = new Array();
+}
+function resetMath(){
+	math = new Array();
+}
+//////////////////////////////////
+
+/////////////mainOperation//////////////
+function calculation(){
+	answer = targets[0];
+	for(var i=0;i<targets.length;i++){
+		if(targets.length>i+1){
+			var calcNum:int = targets[i+1];
+			switch(math[i]){
+				case "+":answer+=calcNum;break;
+				case "-":answer-=calcNum;break;
+				case "*":answer*=calcNum;break;
+				case "/":answer/=calcNum;break;
+			}
+		}
+	}
+	Debug.Log("answer is "+answer);
+}
+////////////////////////////////////////////
